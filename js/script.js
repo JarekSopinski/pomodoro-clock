96834c75d29b3e76a0185a9db69f2aa33bd5f6cc
JarekSopinski <jarek.sopinski@gmail.com>
2018-04-27 21:41:06 +0200

    Changing length of session and breaks

--- js/script.js
+++ js/script.js
@@ -1,9 +1,15 @@
+/*
+TODO:
+1) Help pop-up
+2) Reset button?
+ */
+
 const $timer = $("#js-timer");
 const $displayStatus = $("#js-display-status");
 const $displayTimeLeft = $("#js-display-time-left");
 
-const $displayCompletedPomodoros = $("#js-display-completed");
-const $displayPomodorosLeftToLongBreak = $("#js-display-left-to-long-break");
+const $displayCompletedSessions = $("#js-display-completed");
+const $displaySessionsLeftToLongBreak = $("#js-display-left-to-long-break");
 
 const $increaseSessionLengthBtn = $("#js-increase-session-btn");
 const $decreaseSessionLengthBtn = $("#js-decrease-session-btn");
@@ -20,7 +26,7 @@ const $displayLongBreakLengthBtn = $("#js-display-long-break-length");
 let state = {};
 
 const initialState = {
-    isTimeRunning: false,
+    isTimeRunning: true,
     status: "session",
     sessionLength: 25,
     shortBreakLength: 5,
@@ -28,3 +34,46 @@ const initialState = {
     sessionsCompleted: 0,
     sessionsLeftToLongBreak: 4
 };
+
+const settingsChangeAbortedMsg = "You can't change length while the timer is running. Please, stop the timer first. Keep in mind that changing session's length will restart current session.";
+
+const setInitialState = () => state = $.extend(true, {}, initialState);
+
+const setSessionLength = action => action === "increment" ? state.sessionLength++ : state.sessionLength--;
+const setShortBreakLength = action => action === "increment" ? state.shortBreakLength++ : state.shortBreakLength--;
+const setLongBreakLength = action => action === "increment" ? state.longBreakLength++ : state.longBreakLength--;
+
+const handleSettingsBtn = (sessionOrBreak, action) => {
+
+    if (state.isTimeRunning) { alert(settingsChangeAbortedMsg) }
+
+    else {
+        switch (sessionOrBreak) {
+            case "session":
+                setSessionLength(action);
+                $displaySessionLength.text(state.sessionLength);
+                break;
+            case "shortBreak":
+                setShortBreakLength(action);
+                $displayShortBreakLength.text(state.shortBreakLength);
+                break;
+            case "longBreak":
+                setLongBreakLength(action);
+                $displayLongBreakLengthBtn.text(state.longBreakLength)
+        }
+    }
+
+};
+
+$(document).ready(() => {
+
+    setInitialState();
+
+    $increaseSessionLengthBtn.on("click", () => handleSettingsBtn("session", "increment"));
+    $decreaseSessionLengthBtn.on("click", () => handleSettingsBtn("session", "decrement"));
+    $increaseShortBreakLengthBtn.on("click", () => handleSettingsBtn("shortBreak", "increment"));
+    $decreaseShortBreakLengthBtn.on("click", () => handleSettingsBtn("shortBreak", "decrement"));
+    $increaseLongBreakLengthBtn.on("click", () => handleSettingsBtn("longBreak", "increment"));
+    $decreaseLongBreakLengthBtn.on("click", () => handleSettingsBtn("longBreak", "decrement"))
+
+});
